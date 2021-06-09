0662140c193be79842df1c238c1ce1d89e9cd4a4
Robert <roberttrzaska@outlook.com>
2021-06-09 21:24:09 +0200

    init

---
+++ movie_finder/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app():
+    movie_finder = Flask(__name__)
+
+    from movie_finder.Views.main_views import main_bp
+    movie_finder.register_blueprint(main_bp)
+
+    return movie_finder
+
+
+
+app = create_app()
+
